b0a0af536d2a57021ede355ca7b7984925f9bd84
Nicolas Dextraze <nicolas.dextraze@dexdo.com>
2016-03-11 15:53:33 -0800

    for ClientMessage use pbjs generated script instead of .proto

--- src/messages/clientMessage.js
+++ src/messages/clientMessage.js
@@ -1,7 +1,1 @@
-var path = require('path');
-var ProtoBuf = require('protobufjs');
-var builder = ProtoBuf.loadProtoFile(path.join(__dirname, 'messages.proto'));
-var root = builder.build();
-var ClientMessage = root.EventStore.Client.Messages;
-
-module.exports = ClientMessage;
+module.exports = require('./messages').EventStore.Client.Messages;
