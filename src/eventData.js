8735b23bf91f0f4bf37fd89f9068efb0c9a09d83
Nicolas Dextraze <nicolas.dextraze@adaptechsolutions.ca>
2017-06-05 15:23:27 -0700

    Remove strictness of UUID

--- src/eventData.js
+++ src/eventData.js
@@ -1,6 +1,6 @@
 var uuid = require('uuid');
 
-const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 function isValidId(id) {
   if (typeof id !== 'string') return false;
   return uuidRegex.test(id);
