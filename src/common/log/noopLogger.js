941f671ed22a5c9e6ae9792a8b4ad354bb9b685b
Nicolas Dextraze <nicdex@yahoo>
2019-11-01 11:10:16 -0700

    Fix missing warn in Loggers

--- src/common/log/noopLogger.js
+++ src/common/log/noopLogger.js
@@ -1,7 +1,8 @@
 function NoopLogger() {
 }
-NoopLogger.prototype.error = function() {};
 NoopLogger.prototype.debug = function() {};
 NoopLogger.prototype.info = function() {};
+NoopLogger.prototype.warn = function() {};
+NoopLogger.prototype.error = function() {};
 
 module.exports = NoopLogger;
