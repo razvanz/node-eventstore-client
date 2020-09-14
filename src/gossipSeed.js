f79a0444f6a1a3ed13933f9e9c2964a61a824aa8
maniolias <cedric.rumiano@gmail.com>
2020-09-14 17:29:58 +0200

    feat(cluster): rewrite cluster discovering
    * Discovering process adapted from EventStore scala client
    * Use DNS for first discover but also for reconnoctions (the aim is to be able to reconnect even if all nodes have new IP addresses eg. after rolling update in a cloud environment)
    * Being able to perform proper unit testing

--- src/gossipSeed.js
+++ src/gossipSeed.js
@@ -1,7 +1,8 @@
-function GossipSeed(endPoint, hostName) {
+function GossipSeed(endPoint, hostName, hostHeader) {
   if (typeof endPoint !== 'object' || !endPoint.host || !endPoint.port) throw new TypeError('endPoint must be have host and port properties.');
   this.endPoint = endPoint;
   this.hostName = hostName;
+  this.hostHeader = hostHeader;
   Object.freeze(this);
 }
 
